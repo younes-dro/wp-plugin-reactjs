c6e9f6a1c6cab75f6a213c7c4b034ee467232c2d
younes-dro <dro66_8@hotmail.fr>
2019-08-22 18:27:28 +0100

    Initial commit

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className = "wrap">
+      <h1 className = "wp-heading-inline">congratulations!</h1>
+      <p className="welcome-panel">you are ready to develop a WordPress Plugin with ReactJS.</p>
+      </div>
+    );
+  }
+
+}
+
+export default App;
