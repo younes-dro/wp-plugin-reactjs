68fdca847d42123e1d276e3ac1ff5fb806107fe9
Younes DRO <dro66_8@hotmail.fr>
2024-01-26 11:17:32 +0100

    Refactor: Use Tools menu instead of Settings and update README

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,8 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className = "wrap">
-      <h1 className = "wp-heading-inline">congratulations!</h1>
-      <p className="welcome-panel">you are ready to develop a WordPress Plugin with ReactJS.</p>
+      <h1 className = "wp-heading-inline">Congratulations!</h1>
+      <p>You are ready to develop a WordPress Plugin with ReactJS.</p>
       </div>
     );
   }
